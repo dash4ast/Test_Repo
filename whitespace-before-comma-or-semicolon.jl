897b9110ddcc674574b36104b5066597cf5a8592
dash4ast <124378154+dash4ast@users.noreply.github.com>
2024-02-05 11:04:52 +0100

    Create whitespace-before-comma-or-semicolon.jl

---
+++ whitespace-before-comma-or-semicolon.jl
@@ -0,0 +1,8 @@
+# ruleid: whitespace-before-comma-or-semicolon
+(x  , y) = (1 , 2)
+
+# ruleid: whitespace-before-comma-or-semicolon
+x , y = y , x
+
+# ruleid: whitespace-before-comma-or-semicolon
+[1   ; 2; 3]
